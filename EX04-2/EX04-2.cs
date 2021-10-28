e6f632442a11d79acd10a93d164416110b991b70
BearKS <56060226+BearKS@users.noreply.github.com>
2021-10-29 06:30:56 +0700

    Add EX04V2 and EX05

--- EX04-2/EX04-2.cs
+++ EX04-2/EX04-2.cs
@@ -2,51 +2,50 @@
 using System.Diagnostics;
 using System.Threading;
 
-namespace EX04
+namespace EX04_2
 {
-    class EX04
+    class EX04_2
     {
         private static string x = "";
         private static int exitflag = 0;
         
         //---------------Add Lock---------------//
         private static object _Lock = new object();
-        //-----------Add Check State------------//
-        private static int check = 0;
 
         static void ThReadX()
         {
-            while (exitflag == 0)
-            {
-                lock (_Lock)
-                {   
-                    if (check == 1) //If get input X
-                    {
-                        Console.WriteLine("X = {0}", x);  
-                        check = 0; //Change state for the wait to get input
-                    }
+            while(exitflag == 0)
+            { 
+                Monitor.Enter(_Lock);
+                try
+                {
+                    Monitor.Wait(_Lock); //waits here until thread pulses, releasing the lock
+                    Console.WriteLine("X = {0}", x);
                 }
+                finally
+                {
+                    Monitor.Exit(_Lock);
+                }
+                
             }
             Console.Write("Thread 1 exit");
         }
 
-        static void ThWriteX()
+        static void  ThWriteX()
         {
             string xx;
-            while (exitflag == 0)
+            while(exitflag == 0)
             {
-                lock (_Lock)
+                lock(_Lock)
                 {
+                    Monitor.Pulse(_Lock);
                     Console.Write("Input: ");
                     xx = Console.ReadLine();
-                    if (xx == "exit")
+                    if(xx == "exit")
                         exitflag = 1;
                     else
-                    {
                         x = xx;
-                        check = 1; //Change state for show in ThReadX()
-                    }  
-                }                   
+                }
             }
         }
 
