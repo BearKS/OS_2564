a7d036da5fe1147ad4cfa24ddbeaee941610b89d
BearKS <56060226+BearKS@users.noreply.github.com>
2021-10-29 04:52:21 +0700

    Add EX04
    Activity#1 EX04 Finish

---
+++ EX04/EX04.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace EX04
+{
+    class EX04
+    {
+        private static string x = "";
+        private static int exitflag = 0;
+        
+        //---------------Add Lock---------------//
+        private static object _Lock = new object();
+        //-----------Add Check State------------//
+        private static int check = 0;
+
+        static void ThReadX()
+        {
+            while (exitflag == 0)
+            {
+                lock (_Lock)
+                {   
+                    if (check == 1) //If get input X
+                    {
+                        Console.WriteLine("X = {0}", x);  
+                        check = 0; //Change state for the wait to get input
+                    }
+                }
+            }
+        }
+
+        static void ThWriteX()
+        {
+            string xx;
+            while (exitflag == 0)
+            {
+                lock (_Lock)
+                {
+                    Console.Write("Input: ");
+                    xx = Console.ReadLine();
+                    if (xx == "exit")
+                    {
+                        exitflag = 1;
+                        Console.Write("Thread 1 exit");
+                    }
+                    else
+                    {
+                        x = xx;
+                        check = 1; //Change state for show in ThReadX()
+                    }  
+                }                   
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread A = new Thread(new ThreadStart(ThReadX));
+            Thread B = new Thread(new ThreadStart(ThWriteX));
+           
+            A.Start();
+            B.Start();
+        }
+    }
+}
